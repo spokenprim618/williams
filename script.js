8a1dfb10b99fce4a8d15e5fd21ed586f88efd665
unknown <williamv1905@mhbx.org>
2021-09-16 13:47:07 -0400

    fin

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// https://store.playstation.com/en-us/product/UP9000-CUSA10237_00-HRZCE00000000000/
-// link for horizon above 
-// https://store.playstation.com/en-us/product/UP9000-CUSA07408_00-00000000GODOFWAR/
-// link to GOW
-// https://store.playstation.com/en-us/product/UP2002-CUSA01163_00-ROCKETLEAGUENA01/
-// link to Rocket leauge 
-// https://store.playstation.com/en-us/category/cb1f8320-c804-44eb-bd95-a92490a510d9/1?emcid=pa-pl-415257&gclid=Cj0KCQjwu7OIBhCsARIsALxCUaM_iZuKeClISeFiQXOppxouPsA-imz4qT85gZoOFkr5TIHiSCQ6YmoaAgv6EALw_wcB&gclsrc=aw.ds
-// link to playstation store
-
-
 let visibilityState = 'hidden';
 const contentDiv = document.querySelector('#textsss');
 const button = document.querySelector('#buttion');
@@ -57,8 +47,3 @@ bwutton.addEventListener('click', dog)
 }
 
  bsutton.addEventListener('click', cat)
-
-
-
-
-
